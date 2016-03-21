15e36320fb7986d12c4938ed87e8ede332695bce
sacwin <anwin.varghese@yahoo.com>
2016-03-21 14:51:32 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,6 +38,10 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+package { 'ntp':
+  ensure => present,
+}
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
