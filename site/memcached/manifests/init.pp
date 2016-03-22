85a6325fa7764f0bcc175acb1e648f81a3cb39d8
sacwin <anwin.varghese@yahoo.com>
2016-03-22 14:18:51 +0530

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,19 @@
+
+file { '/etc/sysconfig/memcached' :
+  ensure => 'present',
+  owner => 'root',
+  group => 'root',
+  mode => '0755',
+  source => 'puppet:///modules/memcached/memchached',
+  require => Package['memcached'],
+}
+
+package { 'memchaced' :
+ ensure => 'present',
+}
+
+service { 'memchached' :
+  ensure => running,
+  enabled => true,
+  subscribe => File['/etc/sysconfig/memcached'],
+}
