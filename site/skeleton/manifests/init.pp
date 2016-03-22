742e663f76c18c197a4701263fc3b714fe1c8867
sacwin <anwin.varghese@yahoo.com>
2016-03-22 11:40:37 +0530

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -12,7 +12,7 @@ class skeleton {
     owner => 'root',
     group => 'root',
     mode => '0755',
-    source => "puppet:///skeleton/files/bashrc"
+    source => "puppet:///site/skeleton/files/bashrc"
   }
   
 }
