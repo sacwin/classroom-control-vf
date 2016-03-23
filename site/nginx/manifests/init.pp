9a58519a692455e880c2878c1197a495623d6562
sacwin <anwin.varghese@yahoo.com>
2016-03-23 10:00:37 +0530

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,48 +1,42 @@
 class nginx {
- 
-  file { "/var/www":
-    ensure => 'directory',
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-    require => Package['nginx'],
-  }
 
-  file { "/etc/nginx/nginx.conf":
-    ensure => 'file',
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-    source => "puppet:///modules/nginx/nginx.conf",
-    require => Package['nginx'],
-  }
+ File {
+  owner => 'root',
+  group => 'root',
+  mode => '0755',
+ }
+ 
+ file { "/var/www":
+  ensure => 'directory',
+  require => Package['nginx'],
+ }
+
+ file { "/etc/nginx/nginx.conf":
+  ensure => 'file',
+  source => "puppet:///modules/nginx/nginx.conf",
+  require => Package['nginx'],
+ }
   
-  file { "/etc/nginx/conf.d/default.conf":
-    ensure => 'file',
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-    source => "puppet:///modules/nginx/default.conf",
-    require => Package['nginx'],
-  }
+ file { "/etc/nginx/conf.d/default.conf":
+  ensure => 'file',
+  source => "puppet:///modules/nginx/default.conf",
+  require => Package['nginx'],
+ }
   
-    file { "/var/www/index.html":
-    ensure => 'file',
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-    source => "puppet:///modules/nginx/index.html",
-    require => Package['nginx'],
-  }
+ file { "/var/www/index.html":
+  ensure => 'file',
+  source => "puppet:///modules/nginx/index.html",
+  require => Package['nginx'],
+ }
   
-  package { 'nginx' :
-   ensure => 'present',
-  }
+ package { 'nginx' :
+  ensure => 'present',
+ }
   
-  service { 'nginx' :
-    ensure => running,
-    enable => true,
-    subscribe => File['/etc/nginx/nginx.conf'],
-  }
+ service { 'nginx' :
+  ensure => running,
+  enable => true,
+  subscribe => File['/etc/nginx/nginx.conf'],
+ }
 
 }
